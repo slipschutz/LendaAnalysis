65e2b945c4892cc93eca76e50aa7956b6a67c10b
Samuel Lipschutz <lipschut@bluegill.nscl.msu.edu>
2013-12-23 15:23:53 -0500

    Added a bunch new histograms.  Added energies for particular Muliplicity and a coresponding one with no OverFlows These are implemented as two dimensional vectors.

--- LendaAnalysis.C
+++ LendaAnalysis.C
@@ -18,7 +18,7 @@
 #include <vector>
 #include <sstream>
 #include <TH1F.h>
-
+#include <cmath>
 int main(int argc, char **argv){
 
   vector <string> inputs;
@@ -81,56 +81,112 @@ int main(int argc, char **argv){
   outFile = new TFile(temp.str().c_str(),"recreate");
 
 
-  //////////////////////////////////////////////
+  //////////////////////////////////////////////  //////////////////////////////////////////////  //////////////////////////////////////////////  //////////////////////////////////////////////
   //Declare A Bunch Of histograms
-  //////////////////////////////////////////////
+  //////////////////////////////////////////////  //////////////////////////////////////////////  //////////////////////////////////////////////  //////////////////////////////////////////////
   Int_t NumOfChannelsInModule=16;
-  
-  
 
   vector <TH1F*> EnergiesRaw(NumOfChannelsInModule);
+  vector <TH1F*> EnergiesNoOFs(NumOfChannelsInModule);  
+
+  vector < vector <TH1F*> > EnergiesRawForN; //outer vector for various Multiplicities
+  vector < vector <TH1F*> > EnergiesNoOFsForN; //outer vector for various Multiplicities
+
+  //Defualt Binning For Energy Histograms
   Double_t ERawBinning[3];
   ERawBinning[0]=1000;
   ERawBinning[1]=0;
   ERawBinning[2]=40000;
   
-  
+  //Raw Channels Histogram
   TH1F * ChannelsRaw = new TH1F("ChannelsRaw","ChannelsRaw",16,0,15);
+
+  //Energy Cut settings for Channel histograms
+  Double_t NumOfChannelECuts = 20;
+  Double_t MaxEnergy = 7000;
+  vector <TH1F *> ChannelsECut(NumOfChannelECuts);
+
   TH1F * Multiplicity = new TH1F("Multiplicity","Multiplicity",16,0,15);
-  
+
+
   stringstream nameStream;
-  
   for (int i=0;i<NumOfChannelsInModule;i++){
+    
     nameStream.str("");
     nameStream<<"ERaw"<<i;
-    EnergiesRaw[i] = new TH1F(nameStream.str().c_str(),nameStream.str().c_str(),
-			      ERawBinning[0],ERawBinning[1],ERawBinning[2]);
-
+    EnergiesRaw[i]= new TH1F(nameStream.str().c_str(),nameStream.str().c_str(),
+				 ERawBinning[0],ERawBinning[1],ERawBinning[2]);
+    nameStream.str("");
+    nameStream<<"ENoOverFlows"<<i;
+    EnergiesNoOFs[i]= new TH1F(nameStream.str().c_str(),nameStream.str().c_str(), // use same binning as raw histos
+			       ERawBinning[0],ERawBinning[1],ERawBinning[2]);
   }
+
 
   
 
+  Int_t MaxMultiplicity=6;
+  EnergiesRawForN.resize(MaxMultiplicity);
+  EnergiesNoOFsForN.resize(MaxMultiplicity);
+
+
+  //Declare things histograms for particular multiplicities
+  for (int j=0;j<MaxMultiplicity;j++){
+    for (int i=0;i<NumOfChannelsInModule;i++){
+      nameStream.str("");
+      nameStream<<"ERaw"<<i<<"N"<<j+1;
+      EnergiesRawForN[j].push_back( new TH1F(nameStream.str().c_str(),nameStream.str().c_str(),
+					 ERawBinning[0],ERawBinning[1],ERawBinning[2]));
+      nameStream.str("");
+      nameStream<<"ENoOverFlows"<<i<<"N"<<j+1;
+      EnergiesNoOFsForN[j].push_back( new TH1F(nameStream.str().c_str(),nameStream.str().c_str(), // use same binning as raw histos
+					   ERawBinning[0],ERawBinning[1],ERawBinning[2]));
+
+    }
+  }
+
+
+  for (int i=0;i<NumOfChannelECuts;i++){
+    nameStream.str("");
+    nameStream<<"ChannelsECut_"<<floor((i/NumOfChannelECuts)*MaxEnergy);
+
+    ChannelsECut[i] = new  TH1F(nameStream.str().c_str(),nameStream.str().c_str(),16,0,15);
+  }
 
 
 
-  //////////////////////////////////////////////
+
+  //////////////////////////////////////////////  //////////////////////////////////////////////  //////////////////////////////////////////////  //////////////////////////////////////////////
   //Main Analysis Loop
-  //////////////////////////////////////////////
+  //////////////////////////////////////////////  //////////////////////////////////////////////  //////////////////////////////////////////////  //////////////////////////////////////////////
 
   LendaEvent * theEvent = new LendaEvent();
   inT->SetBranchAddress("Event",&theEvent);
   for (Long64_t jentry=0;jentry<nentry;jentry++){ // LOOP OVER ALL EVENTS
     inT->GetEntry(jentry);
-    for (int i=0;i<theEvent->N;i++){ //Loop Over all Channel Firings in the Event
 
-      
-      ///Fill Raw Energy Histograms
+    for (int i=0;i<theEvent->N;i++){ //Loop Over all Channel Firings in the Event
+      //Fill Raw energy Histogram
       EnergiesRaw[theEvent->channels[i]]->Fill(theEvent->energies[i]);
       
+      ///Fill Raw Energy Histograms for particlar Multiplicities
+      EnergiesRawForN[theEvent->N-1][theEvent->channels[i]]->Fill(theEvent->energies[i]);
+      
+      if (theEvent->OverFlows[i] == false){
+	//Fill Energies no OFs for particular Multiplicities
+	EnergiesNoOFsForN[theEvent->N-1][theEvent->channels[i]]->Fill(theEvent->energies[i]);
+	//Fill Raw No overflow histogram
+	EnergiesNoOFs[theEvent->channels[i]]->Fill(theEvent->energies[i]);
+      }
       ///Fill Raw Channels Histogram
       ChannelsRaw->Fill(theEvent->channels[i]);
-
-
+      
+      //Fill Channels Histogram with energy cut
+      for (int a=0;a<ChannelsECut.size();a++){
+	if (theEvent->energies[i]>(double(a)/ChannelsECut.size())*MaxEnergy){
+	  ChannelsECut[a]->Fill(theEvent->channels[i]);
+	}
+      }
     }//End Loop over Channels In Event
     
     Multiplicity->Fill(theEvent->N);
@@ -142,11 +198,27 @@ int main(int argc, char **argv){
   }//End Main Loop over Everything
 
 
+  //////////////////////////////////////////////
+  //Write all the histograms to file
+  //////////////////////////////////////////////
+
+  for (int i=0;i<EnergiesNoOFsForN.size();i++){
+    for (int j=0;j<EnergiesNoOFsForN[i].size();j++){
+	EnergiesNoOFsForN[i][j]->Write();	
+	EnergiesRawForN[i][j]->Write();
+      }
+  }
+  
 
   for (int i=0;i<EnergiesRaw.size();i++){
     EnergiesRaw[i]->Write();
+    EnergiesNoOFs[i]->Write();
+  }
+  for (int i=0;i<ChannelsECut.size();i++){
+    ChannelsECut[i]->Write();
   }
   ChannelsRaw->Write();
+
   Multiplicity->Write();
   outFile->Close();
   
